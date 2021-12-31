735535a3ee7156bbca296a06a683685d58c32bfb
snailman06 <96637190+snailman06@users.noreply.github.com>
2021-12-31 12:28:14 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,28 +1,28 @@
 /*p = principal
  * i = interest
- * years = client input for years
+ * y = client input for years
  * target = completion year
  * plus = principal plus interest
- * rate = interest input*/
+ * r = interest input
+ * rv = interest after click*/
 function compute() {
-var plus = (p+i);
+    var plus = (p+i);
     document.getElementById('plus').innerHTML = plus;
-var p = parseInt(document.getElementById('p').value);
-var rate = document.getElementById('rate').value;
-var years = document.getElementById("years").value;
-var target = (new Date().getFullYear() + parseInt(years));
+    var p = parseInt(document.getElementById('p').value);
+    var r = document.getElementById('r').value;
+    var y = document.getElementById('y').value;
+    var target = (new Date().getFullYear() + parseInt(y));
     document.getElementById('target').innerHTML = target;
 
     /*Simple Interest Formula =
-  principal*rate*years/100*/
-var i = (p*years*rate)/100;
+  principal*interest rate*years/100*/
+    var i = (p*y*r)/100;
     document.getElementById('i').innerHTML = i;
-    
-  
+
+
 }
-function updateRate()  {/*interest change*/
-    var rateval = document.getElementById("rate").value;
-    document.getElementById("rate_val").innerText=rateval;
+function uR() {/*interest change*/
+    var rv = document.getElementById('r').value;
+    document.getElementById('rv').innerText = rv;
 }
 
-        
