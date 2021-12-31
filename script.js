81eece3492dfa826cb2807cf00235af205a5d5f9
snailman06 <96637190+snailman06@users.noreply.github.com>
2021-12-31 11:47:56 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+/*p = principal
+ * i = interest
+ * years = client input for years
+ * target = completion year
+ * plus = principal plus interest
+ * rate = interest input*/
+function compute() {
+var plus = (p+i);
+    document.getElementById('plus').innerHTML = plus;
+var p = parseInt(document.getElementById('p').value);
+var rate = document.getElementById('rate').value;
+var years = document.getElementById("years").value;
+var target = (new Date().getFullYear() + parseInt(years));
+    document.getElementById('target').innerHTML = target;
+
+    /*Simple Interest Formula =
+  p*r*t/100*/
+var i = (p*years*rate)/100;
+    document.getElementById('i').innerHTML = i;
+    
+  
+}
+function updateRate()  {/*interest change*/
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval;
+}
+
+        
